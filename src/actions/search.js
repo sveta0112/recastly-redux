dc25b27be2b36fcbd7453a5d1848c893dcf369e2
sveta0112 <sveta0112@hotmail.com>
2018-10-03 16:26:28 -0700

    completed actions

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,7 +5,13 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+  return (dispatch) => {
+    searchYouTube({key: YOUTUBE_API_KEY, query: q}, (videos) => {
+      dispatch(changeVideo(videos[0]));
+      dispatch(changeVideoList(videos));
+
+    })
+  }
   //TODO:  Write an asynchronous action to handle a video search!
 };
 
--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,9 @@
-var changeVideoList = (videos) => ({
+var changeVideoList = (videos) => (
   //TODO:  Return some action object to change the list of videos in VideoList.
-});
+  {
+    type: 'CHANGE_VIDEO_LIST',
+    videos: videos
+  }
+);
 
 export default changeVideoList;
